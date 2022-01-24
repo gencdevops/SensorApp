f60c722b580dfbef718a730ced1f56ed05920b38
EXT02D47972 <taner.genc@consultant.turkcell.com.tr>
2022-01-24 13:16:23 +0300

    Initial commit

---
+++ src/main/java/com/sensorapp/sensorapp/SensorAppApplication.java
@@ -0,0 +1,13 @@
+package com.sensorapp.sensorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SensorAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sensorapp/sensorapp/SensorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sensorapp.sensorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
