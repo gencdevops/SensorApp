f5213bcaf992969916c319610195114930fdf8e6
EXT02D47972 <gencdevops@gmail.com>
2022-01-25 20:21:31 +0300

    environment
    enviroment config eklendi

---
+++ src/main/java/com/sensorapp/sensorapp/configuration/InitialConfig.java
@@ -0,0 +1,17 @@
+package com.sensorapp.sensorapp.configuration;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.WebApplicationInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+/*
+@Component
+public class InitialConfig implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        servletContext.setInitParameter("spring.profiles.active", "dev");
+    }
+}
+*/
